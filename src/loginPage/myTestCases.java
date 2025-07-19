5d46f9ecf979c208523ffc2c204c986efc62c509
Feras Aljaber <Feras.Aljaber2002.com>
2025-07-19 14:48:23 +0300

    update

--- src/loginPage/myTestCases.java
+++ src/loginPage/myTestCases.java
@@ -1,5 +1,7 @@
 package loginPage;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,6 +15,8 @@ public class myTestCases {
 	String theURL = "https://automationteststore.com/";
 	String signupPage = "https://automationteststore.com/index.php?rt=account/create";
 	
+	Random rand = new Random();
+	
 	@BeforeTest
 	public void mySetup() {
 		
@@ -22,13 +26,65 @@ public class myTestCases {
 	
 	
 	@Test(priority = 1)
-	public void signup() {
+	public void signup()  {
 		
 		driver.navigate().to(signupPage);
-		WebElement firstName = driver.findElement(By.xpath("//*[@id=\"AccountFrm_firstname\"]"));
 		
-		firstName.sendKeys("ahmad"); 
+		//elements
+		WebElement firstnameInput = driver.findElement(By.xpath("//input[@name=\"firstname\"]"));
+		WebElement lastNameInput = driver.findElement(By.id("AccountFrm_lastname"));
+		WebElement emailInput = driver.findElement(By.id("AccountFrm_email"));
+		WebElement teleInput = driver.findElement(By.id("AccountFrm_telephone"));
+		WebElement faxInput = driver.findElement(By.id("AccountFrm_fax"));
+		WebElement companyInput = driver.findElement(By.id("AccountFrm_company"));
+		WebElement address1Input = driver.findElement(By.id("AccountFrm_address_1"));
+		WebElement address2Input = driver.findElement(By.id("AccountFrm_address_2"));
+		WebElement cityInput = driver.findElement(By.id("AccountFrm_city"));
 		
+		WebElement PostalCodeInput = driver.findElement(By.id("AccountFrm_postcode"));
+		WebElement loginNameInput = driver.findElement(By.id("AccountFrm_loginname"));
+		WebElement passwordInput = driver.findElement(By.id("AccountFrm_password"));
+		WebElement passwordConfirmInput = driver.findElement(By.id("AccountFrm_confirm"));
+		WebElement agreebox = driver.findElement(By.id("AccountFrm_agree"));
+		WebElement ContinueButton = driver.findElement(By.cssSelector("button[title='Continue']"));
+		WebElement CountrySelect = driver.findElement(By.id("AccountFrm_country_id"));
+		WebElement StateSelect = driver.findElement(By.id("AccountFrm_zone_id"));
+		
+		//data
+		String [] firstNames = {"ahmad" , "anas" , "omar" ,"ali","amira","mais"};
+		int randomIndexForFirstNames = rand.nextInt(firstNames.length);
+		String randomFirstName = firstNames[randomIndexForFirstNames];
+		
+		String [] lastNames = {"alaa" , "saif","abdullah","hamzeh","marwan" };
+		int randomIndexForlastNames = rand.nextInt(lastNames.length);
+		String randomLastName = lastNames[randomIndexForlastNames];
+		
+		int randomNumberForTheEmail = rand.nextInt(7000);
+		String email = randomFirstName+randomLastName+randomNumberForTheEmail+"gmail.com";
+		String telephone = "9624545455";
+		String fax = "9624545455";
+		String company = "abc";
+		String address1 = "Amman tlaaelali";
+		String address2 = "Amman shafaBaadran";
+		String city = "Amman";
+		String PostalCode = "3817";
+		String password = "1234";
+
+		
+		//actions
+		firstnameInput.sendKeys(randomFirstName);
+		lastNameInput.sendKeys(randomLastName);
+		emailInput.sendKeys(email);
+		teleInput.sendKeys(telephone);
+		faxInput.sendKeys(fax);
+		companyInput.sendKeys(company);
+		address1Input.sendKeys(address1);
+		address2Input.sendKeys(address2);
+		cityInput.sendKeys(city);
+		PostalCodeInput.sendKeys(PostalCode);
+		loginNameInput.sendKeys(randomFirstName+randomLastName+randomNumberForTheEmail);
+		passwordInput.sendKeys(password);
+		passwordConfirmInput.sendKeys(password);
 		
 		
 	}
